e47c463ba04201f85ccc052f6fc0497babf1c66e
Alexander Huynh <Alexander Huynh>
2017-02-16 22:48:16 -0800

    beautified JS, added IIFE

--- main.js
+++ main.js
@@ -1,38 +1,37 @@
 (function() {
-  const inputs = document.querySelectorAll('.controls input');
+    const inputs = document.querySelectorAll('.controls input');
 
-function changeImg(url) {
-    document.querySelector('img').setAttribute('src', url);
-}
-
-function handleUpdate() {
-    //Padding and blur require px suffix, but other properties do not.
-    const suffix = this.dataset.sizing || '';
-    //Handle case for opacity because values are 0 to 1
-    if (this.id === 'opacity') {
-        document.documentElement.style.setProperty(`--${this.name}`, this.value / 100);
-        return;
+    function changeImg(url) {
+        document.querySelector('img').setAttribute('src', url);
     }
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-}
-//Add Event Listener to each scroller
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
 
-//Provide a random Image
-function randomImg() {
-    var randomNum = Math.random() * 10;
-    document.querySelector('img').setAttribute('src', `https://source.unsplash.com/random/800x${600 + randomNum}`)
-}
+    function handleUpdate() {
+        //Padding and blur require px suffix, but other properties do not.
+        const suffix = this.dataset.sizing || '';
+        //Handle case for opacity because values are 0 to 1
+        if (this.id === 'opacity') {
+            document.documentElement.style.setProperty(`--${this.name}`, this.value / 100);
+            return;
+        }
+        document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    }
+    //Add Event Listener to each scroller
+    inputs.forEach(input => input.addEventListener('change', handleUpdate));
 
-//When submit is clicked, change the image
-var imgInput = document.getElementById('newImg');
-document.getElementById('changeImg').addEventListener('click', function(){
-  changeImg(imgInput.value);
-});
+    //Provide a random Image
+    function randomImg() {
+        var randomNum = Math.random() * 10;
+        document.querySelector('img').setAttribute('src', `https://source.unsplash.com/random/800x${600 + randomNum}`)
+    }
 
-//When random is clicked, provide a random image
-document.getElementById('randomImg').addEventListener('click', function(){
-  randomImg();
-});
+    //When submit is clicked, change the image
+    var imgInput = document.getElementById('newImg');
+    document.getElementById('changeImg').addEventListener('click', function() {
+        changeImg(imgInput.value);
+    });
+
+    //When random is clicked, provide a random image
+    document.getElementById('randomImg').addEventListener('click', function() {
+        randomImg();
+    });
 })();
-
